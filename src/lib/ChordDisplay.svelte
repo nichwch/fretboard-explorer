1d821858caa663c4d4479468b2b396f09ba655c4
nichwch <nichwch@gmail.com>
2023-04-17 18:28:12 -0700

    styling

--- src/lib/ChordDisplay.svelte
+++ src/lib/ChordDisplay.svelte
@@ -1,29 +1,59 @@
 <script lang="ts">
   import type { Chord } from './types'
   export let chord: Chord
+  chord.reverse()
 </script>
 
 <div id="parent">
-  <div class="line" />
-  <div class="line" />
-  <div class="line" />
-  <div class="line" />
-  <div class="line" />
-  <div class="line" />
-  {#each chord as note}
-    <div>{note}</div>
-  {/each}
+  <div class="column">
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+  </div>
+  <div class="note-column">
+    {#each chord as note}
+      <div class="note" class:is_zero={note === 0}>{note}</div>
+    {/each}
+  </div>
+  <div class="column">
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+    <div class="line" />
+  </div>
 </div>
 
 <style>
   #parent {
     height: 100px;
-    display: inline-block;
+    display: inline-flex;
   }
   .line {
     display: block;
     width: 15px;
     height: 16.66%;
     border-bottom: 1px solid black;
+  }
+
+  .note {
+    display: block;
+    height: 16.66%;
+  }
+
+  .is_zero {
+    opacity: 0.5;
+  }
+
+  .note {
+    font-weight: bold;
+  }
+
+  .note-column {
+    margin-top: 10%;
   }
 </style>
